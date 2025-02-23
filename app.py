b779aca0319f49411f4a5762b10b4b399319f4e3
afreen sultana <nayeemataj024@gmail.com>
2025-02-23 13:37:06 +0530

    local host

--- app.py
+++ app.py
@@ -133,4 +133,5 @@ def download_image(filename):
     return "File not found", 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+
